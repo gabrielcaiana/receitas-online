c8ea1f27f4c10a3cf71bc080204cd9289eb7b6fb
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-17 07:30:13 -0300

    chore: update readme

---
+++ backend/src/api/categorie/controllers/categorie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  categorie controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::categorie.categorie');
---
+++ backend/src/api/categorie/services/categorie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * categorie service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::categorie.categorie');
