e37ef3600152bec66e3850a76947f7d0b2d8e271
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-21 19:53:45 -0300

    chore: init layout default

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="h-screen bg-gray-100">
+    <div class="flex gap-10">
+      <Sidebar />
+
+      <main class="w-auto p-10">
+        <nuxt />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppLayout',
+}
+</script>
