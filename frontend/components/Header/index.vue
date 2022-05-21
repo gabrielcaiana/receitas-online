22e67f1f5957bab920cad46f4d1cee812545ce8b
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-21 20:31:33 -0300

    chore: update header

--- frontend/components/Header/index.vue
+++ frontend/components/Header/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="w-auto shadow-md shadow-gray-500 bg-white h-20"></div>
+  <div
+    class="w-auto shadow-md shadow-gray-500 bg-white h-20 flex items-center px-10"
+  >
+    <div class="flex justify-between flex-auto">
+      <InputSearch />
+      <Avatar />
+    </div>
+  </div>
 </template>
 
 <script>
