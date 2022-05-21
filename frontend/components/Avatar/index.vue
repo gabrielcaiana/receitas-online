0978c7e2e5b93bcdbd31aef817182a98213f1062
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-21 20:31:49 -0300

    chore: create avatar component

---
+++ frontend/components/Avatar/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex gap-4 items-center cursor-pointer">
+    <Icon name="user" color="#333333" />
+    <span>Gabriel Caiana</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppAvatar',
+}
+</script>
