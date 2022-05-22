9d6862e8504d53b8ec06a7abed506c01e5c1e52a
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-22 19:26:54 -0300

    chore: update logo

--- frontend/components/Logo/index.vue
+++ frontend/components/Logo/index.vue
@@ -1,7 +1,7 @@
 <template>
   <nuxt-link to="/">
     <span class="font-bold text-2xl text-gray-700 cursor-pointer"
-      ><span class="text-red-500">Receitas</span> online</span
+      ><span class="text-red-500">Receitas</span> Online</span
     >
   </nuxt-link>
 </template>
