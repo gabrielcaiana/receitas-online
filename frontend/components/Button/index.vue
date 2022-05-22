4d457f6f085984305d2cdb88f9f65220cf62c542
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-21 22:12:21 -0300

    chore: create button component

---
+++ frontend/components/Button/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    type="submit"
+    class="text-white absolute right-2.5 bottom-2.5 bg-red-500 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+    v-text="label"
+  ></button>
+</template>
+
+<script>
+export default {
+  name: 'AppButton',
+
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
--- frontend/components/InputSearch/index.vue
+++ frontend/components/InputSearch/index.vue
@@ -18,12 +18,7 @@
         placeholder="Pesquise por uma receita"
         required
       />
-      <button
-        type="submit"
-        class="text-white absolute right-2.5 bottom-2.5 bg-red-500 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
-      >
-        Pesquisar
-      </button>
+      <Button label="Pesquisar" />
     </div>
   </div>
 </template>
