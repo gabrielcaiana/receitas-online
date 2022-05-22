8e1ba3df3adda58969b6e3a4e79c4ba23945d468
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-22 14:50:04 -0300

    chore: add fetchState to get status

--- frontend/components/Menu/index.vue
+++ frontend/components/Menu/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="bg-white shadow-md shadow-gray-500">
     <hr />
-    <ul class="flex gap-8 items-center justify-center p-4">
+
+    <p v-if="$fetchState.pending">Carregando...</p>
+    <p v-else-if="$fetchState.error">Falha ao carregar as categorias</p>
+
+    <ul v-else class="flex gap-8 items-center justify-center p-4">
       <li
         v-for="(menu, index) in menus"
         :key="index"
@@ -22,17 +26,13 @@ export default {
   }),
 
   async fetch() {
-    try {
-      const client = this.$apollo.getClient()
+    const client = this.$apollo.getClient()
 
-      const { data } = await client.query({
-        query: categoriesQuery(),
-      })
+    const { data } = await client.query({
+      query: categoriesQuery(),
+    })
 
-      this.menus = data?.categories?.data
-    } catch (error) {
-      console.log(error)
-    }
+    this.menus = data?.categories?.data
   },
 }
 </script>
