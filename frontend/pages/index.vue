f2a2f436b289e5fc5b7cee5d27ff221274c71b58
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-23 21:30:33 -0300

    refactor: li

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,24 +1,25 @@
 <template>
   <div>
     <ul class="flex justify-center flex-wrap gap-8">
-      <li v-for="recipe in recipes" :key="recipe.id" class="w-80">
-        <nuxt-link
-          :to="{
-            name: 'categorias-receita',
-            params: {
-              categorias: recipe.attributes.category.data.attributes.slug,
-              receita: recipe.id,
-            },
-          }"
-        >
-          <Card
-            :image="recipe.attributes.img"
-            :title="recipe.attributes.name"
-            :likes="recipe.attributes.likes"
-            :category="recipe.attributes.category.data.attributes.name"
-          />
-        </nuxt-link>
-      </li>
+      <nuxt-link
+        v-for="recipe in recipes"
+        :key="recipe.id"
+        class="w-80"
+        :to="{
+          name: 'categorias-receita',
+          params: {
+            categorias: recipe.attributes.category.data.attributes.slug,
+            receita: recipe.id,
+          },
+        }"
+      >
+        <Card
+          :image="recipe.attributes.img"
+          :title="recipe.attributes.name"
+          :likes="recipe.attributes.likes"
+          :category="recipe.attributes.category.data.attributes.name"
+        />
+      </nuxt-link>
     </ul>
   </div>
 </template>
