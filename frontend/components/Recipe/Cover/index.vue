3939816f0fbf457e272de566759bb34165a66ca3
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-25 20:34:12 -0300

    chore: create cover

---
+++ frontend/components/Recipe/Cover/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <img class="rounded-lg h-96 w-full object-cover" :src="cover" :alt="name" />
+</template>
+
+<script>
+export default {
+  name: 'RecipeCover',
+
+  props: {
+    cover: {
+      type: String,
+      required: true,
+    },
+
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
