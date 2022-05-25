8ac6efbfb69b8c017308bf6b732a694bae9d1f2c
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-25 20:52:39 -0300

    chore: create formatedTime

--- frontend/components/Recipe/Duration/index.vue
+++ frontend/components/Recipe/Duration/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex gap-3">
     <Icon name="clock" color="#374151" />
-    <span class="text-gray-500" v-text="`${duration} minutos`"></span>
+    <span class="text-gray-500" v-text="`${formatedTime} minutos`"></span>
   </div>
 </template>
 
@@ -13,6 +13,15 @@ export default {
     duration: {
       type: Number,
       required: true,
+    },
+  },
+
+  computed: {
+    formatedTime() {
+      const hours = Math.floor(this.duration / 60)
+      const minutes = this.duration % 60
+      const total = ('0' + hours).slice(-2) + ':' + ('0' + minutes).slice(-2)
+      return total
     },
   },
 }
