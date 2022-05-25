16d8b444c292c59ea19b0f5d5b22f9078586b6a0
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-25 20:35:24 -0300

    chore: create title

---
+++ frontend/components/Recipe/Title/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="text-center text-3xl py-10 text-gray-700" v-text="title"></h1>
+</template>
+
+<script>
+export default {
+  name: 'RecipeTitle',
+
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
