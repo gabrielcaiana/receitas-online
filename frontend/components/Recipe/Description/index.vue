3a5bfec6602f1e751b62e9e10be338a42339dacd
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-25 20:34:25 -0300

    chore: create description

---
+++ frontend/components/Recipe/Description/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2 class="text-xl py-4 text-gray-700">Descrição</h2>
+    <p class="text-gray-500" v-text="description"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RecipeDescription',
+
+  props: {
+    description: {
+      type: String,
+      default: null,
+    },
+  },
+}
+</script>
