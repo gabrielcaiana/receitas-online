169809e38e6722d91bba9d8bc5c74953196276bf
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-25 20:34:34 -0300

    chore: create duration

---
+++ frontend/components/Recipe/Duration/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex gap-3">
+    <Icon name="clock" color="#374151" />
+    <span class="text-gray-500" v-text="`${duration} minutos`"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RecipeDuration',
+
+  props: {
+    duration: {
+      type: Number,
+      required: true,
+    },
+  },
+}
+</script>
