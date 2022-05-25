1a6c42a3f343cbf8b7ef7d8d6408615f05b08e26
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-25 20:35:15 -0300

    chore: create steps

---
+++ frontend/components/Recipe/Steps/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h4 class="text-xl py-4 text-gray-700">Modo de preparo</h4>
+    <ul>
+      <li
+        v-for="(step, index) in steps"
+        :key="index"
+        class="text-gray-500 mb-4"
+        v-text="step"
+      ></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RecipeSteps',
+
+  props: {
+    steps: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
