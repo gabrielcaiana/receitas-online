2b28d5dc9929f94c9cf596c768bd962bd77d670f
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-25 20:34:45 -0300

    chore: create ingredients

---
+++ frontend/components/Recipe/Ingredients/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3 class="text-xl py-4 text-gray-700">Ingredientes</h3>
+    <ul>
+      <li
+        v-for="(ingredient, index) in ingredients"
+        :key="index"
+        class="text-gray-500 mb-4"
+        v-text="ingredient"
+      ></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RecipeIngredients',
+
+  props: {
+    ingredients: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
