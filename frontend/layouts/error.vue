5b9d3c96bfe158d39255436c9a8efd81eca989da
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-25 21:29:35 -0300

    chore: add error layout

---
+++ frontend/layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppError',
+  // eslint-disable-next-line vue/require-prop-types
+  props: ['error'],
+}
+</script>
