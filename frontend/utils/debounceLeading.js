f1f08d83681c393440335f2c7e322f43f13d4c4f
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-27 23:16:14 -0300

    chore: create debounceLeading

---
+++ frontend/utils/debounceLeading.js
@@ -0,0 +1,12 @@
+export const debounceLeading = (func, timeout = 1000) => {
+  let timer
+  return (...args) => {
+    if (!timer) {
+      func.apply(this, args)
+    }
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      timer = undefined
+    }, timeout)
+  }
+}
