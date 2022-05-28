08845c017a6c101f7c2ef16adb182a69311a0d43
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-27 23:16:20 -0300

    chore: create debounce

---
+++ frontend/utils/debounce.js
@@ -0,0 +1,9 @@
+export const debounce = (func, timeout = 1000) => {
+  let timer
+  return (...args) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      func.apply(this, args)
+    }, timeout)
+  }
+}
