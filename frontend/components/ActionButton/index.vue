f1f6b99b83592bd7626211b15e36561e83bc2f1a
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-28 00:10:25 -0300

    chore: add transition to actionButton

--- frontend/components/ActionButton/index.vue
+++ frontend/components/ActionButton/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="rounded-full w-10 h-10 flex items-center justify-center cursor-pointer"
+    class="rounded-full w-10 h-10 flex items-center justify-center cursor-pointer transition duration-300 ease-in-out"
     @click="$emit('click')"
   >
     <Icon :name="icon" :color="color" />
