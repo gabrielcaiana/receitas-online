eb9ccf5187c012dd68cfd45ca17e208f73d6fa8f
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-29 16:07:55 -0300

    chore: add empty state

--- frontend/pages/_categorias/_receita/index.vue
+++ frontend/pages/_categorias/_receita/index.vue
@@ -1,28 +1,35 @@
 <template>
-  <section v-if="recipe" class="w-5/6 m-auto">
-    <RecipeTitle :title="recipe.name" />
-    <div class="grid grid-cols-1 gap-4">
-      <RecipeCover :cover="recipe.img" :name="recipe.name" />
-      <div class="bg-white p-10 rounded-md">
-        <div>
-          <div class="flex gap-10">
-            <RecipeLikes :likes="recipe.likes" />
-            <RecipeDuration :duration="recipe.duration" />
-            <RecipePortions :portions="recipe.portions" />
+  <section class="w-5/6 m-auto">
+    <EmptyState
+      v-if="!recipe"
+      label="A receita não existe, pesquisa por uma nova receita!"
+    />
+
+    <template v-else>
+      <RecipeTitle :title="recipe.name" />
+      <div class="grid grid-cols-1 gap-4">
+        <RecipeCover :cover="recipe.img" :name="recipe.name" />
+        <div class="bg-white p-10 rounded-md">
+          <div>
+            <div class="flex gap-10">
+              <RecipeLikes :likes="recipe.likes" />
+              <RecipeDuration :duration="recipe.duration" />
+              <RecipePortions :portions="recipe.portions" />
+            </div>
+            <Divider />
+
+            <RecipeDescription :description="recipe.description" />
           </div>
+
           <Divider />
 
-          <RecipeDescription :description="recipe.description" />
-        </div>
-
-        <Divider />
-
-        <div class="grid grid-cols-2 gap-4">
-          <RecipeIngredients :ingredients="recipe.ingredients" />
-          <RecipeSteps :steps="recipe.steps" />
+          <div class="grid grid-cols-2 gap-4">
+            <RecipeIngredients :ingredients="recipe.ingredients" />
+            <RecipeSteps :steps="recipe.steps" />
+          </div>
         </div>
       </div>
-    </div>
+    </template>
   </section>
 </template>
 
@@ -31,21 +38,21 @@ export default {
   name: 'RecipePage',
 
   async asyncData({ $strapiApi, params }) {
-    const { recipe } = await $strapiApi.recipesBySlug(params)
+    const response = await $strapiApi.recipesBySlug(params)
 
     return {
-      recipe,
+      recipe: response?.recipe,
     }
   },
 
   head() {
     return {
-      title: this.recipe.name,
+      title: this.recipe?.name,
       meta: [
         {
           hid: 'description',
           name: 'description',
-          content: this.recipe.description,
+          content: this.recipe?.description,
         },
       ],
     }
