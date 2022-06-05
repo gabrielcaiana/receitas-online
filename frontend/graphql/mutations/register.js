c866b118aee54b16a5cfef3238a48d1ad9117e96
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-06-04 23:26:12 -0300

    chore: create mutation

---
+++ frontend/graphql/mutations/register.js
@@ -0,0 +1,20 @@
+import { gql } from 'graphql-tag'
+
+export const registerUserMutation = ({ username, email, password }) => gql`
+  mutation {
+    register(
+      input: {
+        username:"${username}",
+        email:"${email}",
+        password:"${password}",
+      }
+    ) {
+      jwt
+       user {
+        id
+        username
+        email
+      }
+    }
+  }
+`;
