16b90a19950c27b28c04227fb196565960573371
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-06-10 20:07:11 -0300

    chore: create author component

---
+++ frontend/components/molecules/Recipe/Author/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex gap-3">
+    <AtomsIcon name="user" color="#374151" />
+    <p class="text-gray-500" v-text="author"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RecipeAuthor',
+
+  props: {
+    author: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
