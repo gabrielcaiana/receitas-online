c9d970eb4c74a0352a6fc6667612e2434cb949e3
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-06-10 20:06:51 -0300

    chore: update title component

---
+++ frontend/components/atoms/Title/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1
+    :class="['text-3xl  text-gray-700', alignCenter && 'text-center']"
+    v-text="title"
+  ></h1>
+</template>
+
+<script>
+export default {
+  name: 'RecipeTitle',
+
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    alignCenter: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
