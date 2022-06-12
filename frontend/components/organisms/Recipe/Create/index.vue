4ef19a65a21ed7e5aa6d5841727090cef7fb13f7
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-06-12 17:07:54 -0300

    chore: define recipe model

---
+++ frontend/components/organisms/Recipe/Create/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div
+    class="w-full mx-auto bg-white shadow-lg rounded-sm border border-gray-200"
+  >
+    <header
+      class="px-5 py-6 border-b border-gray-100 flex items-center justify-between"
+    >
+      <h2 class="font-semibold text-gray-800">
+        Preencha o formulário para cadastrar a receita
+      </h2>
+    </header>
+
+    <form>
+      <p v-text="recipe.name"></p>
+      <p v-text="recipe.duration"></p>
+      <p v-text="recipe.portions"></p>
+      <p v-text="recipe.img"></p>
+      <p v-text="recipe.description"></p>
+      <p v-text="recipe.ingredients"></p>
+      <p v-text="recipe.steps"></p>
+      <p v-text="recipe.category"></p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RecipeCreate',
+
+  props: {
+    recipe: {
+      type: Object,
+      default: () => ({
+        name: '',
+        duration: 0,
+        portions: 0,
+        img: '',
+        description: '',
+        ingredients: [''],
+        steps: [''],
+        category: 1,
+      }),
+    },
+  },
+}
+</script>
--- frontend/pages/user/new-recipe.vue
+++ frontend/pages/user/new-recipe.vue
@@ -1,11 +1,13 @@
 <template>
-  <section class="m-auto p-10">
+  <section class="m-auto p-10 w-3/5">
     <header class="flex justify-between">
       <AtomsTitle class="mb-10" title="Cadastrar nova receita" />
       <div class="w-40">
-        <AtomsButton label="Voltar" outlined @click="$router.go(-1)" />
+        <AtomsButton label="Voltar" outlined @click="$router.push('/user')" />
       </div>
     </header>
+
+    <OrganismsRecipeCreate />
   </section>
 </template>
 
