3df7f4b802e45d7a3a3edffcfe3672b44c7e57bd
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-06-12 16:29:54 -0300

    chore: mutation create recipe

---
+++ frontend/graphql/mutations/recipes.js
@@ -0,0 +1,37 @@
+import { gql } from 'graphql-tag'
+
+export const createRecipeMutation = ({
+  name,
+  duration,
+  portions,
+  img,
+  description,
+  ingredients,
+  steps,
+  category,
+  author,
+}) => gql`
+  mutation {
+    createRecipe(
+      data: {
+        name: "${name}"
+        duration: ${duration}
+        portions: ${portions}
+        img: "${img}"
+        description: "${description}"
+        ingredients: ${ingredients}
+        steps: ${steps}
+        category: ${category}
+        author: ${author}
+      }
+    ) {
+      data {
+        id
+        attributes {
+          name
+          slug
+        }
+      }
+    }
+  }
+`
